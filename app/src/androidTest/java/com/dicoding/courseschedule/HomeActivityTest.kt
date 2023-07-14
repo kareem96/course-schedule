30681ae31745d064c606d9e328f91b5c6e923d23
Abdul Karim <ak339444@gmail.com>
2023-07-14 16:02:10 +0700

    feat android test UI

---
+++ app/src/androidTest/java/com/dicoding/courseschedule/HomeActivityTest.kt
@@ -0,0 +1,29 @@
+package com.dicoding.courseschedule
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers
+import com.dicoding.courseschedule.ui.add.AddActivity
+import com.dicoding.courseschedule.ui.home.HomeActivity
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class HomeActivityTest {
+
+    @Before
+    fun prepareTest() {
+        ActivityScenario.launch(HomeActivity::class.java)
+        Intents.init()
+    }
+
+    @Test
+    fun onTapGoToAddScreen() {
+        Espresso.onView(ViewMatchers.withId(R.id.action_add)).perform(ViewActions.click())
+        Intents.intended(IntentMatchers.hasComponent(AddActivity::class.java.name))
+    }
+
+}
